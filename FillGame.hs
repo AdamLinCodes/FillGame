5be2abec50d8ac194724839941aaef0280413a57
ADogTheDestroyer <andrewhlin0@gmail.com>
2023-03-29 19:25:18 -0400

    Made fill functions

--- FillGame.hs
+++ FillGame.hs
@@ -1,3 +1,21 @@
+import Debug.Trace
 type Board = [[Char]]
 
-displayBoard :: Board -> Bool
+level1Board :: Board
+level1Board = ["&&&&&&%%%&&&###", "&&&&&&%%%&&&###", "&&&&&&%%%&&&###", "&&&$$$%%%&&&###"]
+
+displayBoard :: Board -> Board
+displayBoard [] = ["End"]
+displayBoard (h:t) = trace h displayBoard t
+
+fillRight :: [Char] -> Char -> Char -> [Char]
+fillRight [] _ _ = []
+fillRight (h:t) c1 c2
+  | h == c1 = c2 : (fillRight t c1 c2)
+  | otherwise = (h:t)
+
+fill :: Board -> Char -> Char -> Board
+fill [] _ _ = []
+fill (h:t) c1 c2
+  | (head h) == c1 = (fillRight h c1 c2) : (fill t c1 c2)
+  | otherwise = (h:t)
